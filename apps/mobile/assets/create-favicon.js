cc68d009e79579f00e77db6e3000ff8fb1970605
JoseLarrainCa <joselarrain.ca@gmail.com>
2026-01-25 22:43:53 -0300

    chore: remove mobile app, migrated to Next.js PWA (apps/web)
    Mobile React Native code available in git history if needed.

--- apps/mobile/assets/create-favicon.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// Minimal valid PNG - 32x32 coral colored square
-const png = Buffer.from('iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIklEQVR4nGNgGAWjYBSMglEwCkbBKBgFo2AUjIJRQE8AAAU4AAHLNKOrAAAAAElFTkSuQmCC', 'base64');
-
-const faviconPath = path.join(__dirname, 'favicon.png');
-fs.writeFileSync(faviconPath, png);
-console.log('Created favicon.png');
--- apps/mobile/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { Modal, ConfirmModal, AlertModal } from './Modal';
-export { NetworkError } from './NetworkError';
-export { default as ScreenHeader } from './ScreenHeader';
